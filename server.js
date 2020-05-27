f58b59d72870688992d44d9ec5846ec574dd2dec
JBailey94 <jessebailey99@gmail.com>
2020-05-27 14:56:44 -0500

    Fixed server issues

--- server.js
+++ server.js
@@ -8,14 +8,26 @@ import handleRegister from './controllers/register.js';
 import handleProfileGet from './controllers/profile.js';
 import { handleImage, handleApiCall } from './controllers/image.js';
 
+// for heroku
 // setup database connection using knex
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//       connectionString : process.env.DATABASE_URL,
+//       ssl: true   
+//     }
+// });
+
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true   
+  client: 'pg',
+    connection: {
+      host : 'localhost',
+      user : 'postgres',
+      password : '',
+      database : 'face'
     }
 });
+
 
 // creates the express application
 const app = express();
@@ -33,7 +45,13 @@ app.put('/image', (req, res) => { handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { handleApiCall(req, res)})
 
 // create the listen port
-const PORT = process.env.PORT;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
 });
+
+// for heroku
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//     console.log(`App is listening on port ${PORT}`);
+// });
